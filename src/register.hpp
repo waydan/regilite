e7435856e2a861e44bdd8052ad63efa16006af33
Daniel Way <p.waydan@gmail.com>
2021-05-17 05:32:32 +0000

    Enable single field write and clear

--- src/register.hpp
+++ src/register.hpp
@@ -1,8 +1,30 @@
 #ifndef REGISTER_HPP
 #define REGISTER_HPP
 
+#include <cstdint>
+
 namespace regilite {
-struct Register {};
+
+
+class Field
+{
+    std::uint32_t value_;
+
+  public:
+    constexpr explicit Field(std::uint32_t value) noexcept : value_(value) {}
+
+    constexpr auto value() const noexcept { return value_; };
+};
+
+
+class Register
+{
+    std::uint32_t raw_;
+
+  public:
+    auto write(Field f) noexcept -> void { raw_ = f.value(); }
+};
+
 } // namespace regilite
 
 
--- test/read_write_unit.cpp
+++ test/read_write_unit.cpp
@@ -1,6 +1,7 @@
 #include "CppUTest/TestHarness.h"
 #include "register.hpp"
 
+#include <cstdint>
 #include <type_traits>
 
 regilite::Register test_register;
@@ -8,6 +9,29 @@ regilite::Register test_register;
 static_assert(std::is_standard_layout<decltype(test_register)>::value,
               "Register<> type must always be standard layout.");
 
-TEST_GROUP(EmptyFirstTest){};
+std::uint32_t& register_view{*reinterpret_cast<std::uint32_t*>(&test_register)};
 
-TEST(EmptyFirstTest, RunTest) {}
+TEST_GROUP(WriteToRegister)
+{
+    void setup() { register_view = 0u; };
+};
+
+TEST(WriteToRegister, SetOneBit)
+{
+    test_register.write(regilite::Field{1});
+    LONGS_EQUAL(0x1, register_view);
+}
+
+TEST(WriteToRegister, ClearOneBit)
+{
+    register_view = 1u;
+    test_register.write(regilite::Field{0});
+    LONGS_EQUAL(0x0, register_view);
+}
+
+TEST(WriteToRegister, SetTwoAdjacentFields)
+{
+    register_view = 1u;
+    test_register.write(regilite::Field{0});
+    LONGS_EQUAL(0x0, register_view);
+}
