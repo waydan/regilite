29312860b06e740374d03f9bc2f9f08b2eabb655
Daniel Way <p.waydan@gmail.com>
2021-05-18 04:56:58 +0000

    Write multiple fields simultaneously

--- src/register.hpp
+++ src/register.hpp
@@ -2,27 +2,70 @@
 #define REGISTER_HPP
 
 #include <cstdint>
+#include <type_traits>
 
 namespace regilite {
 
+template <typename Int>
+constexpr auto lsb(Int value) noexcept
+    -> std::enable_if_t<std::is_integral<Int>::value, int>
+{
+    auto x = static_cast<std::make_unsigned_t<Int>>(value);
+    int lsb = sizeof(x) * 8;
+    for (; x != 0; x <<= 1u)
+        --lsb;
+    return lsb;
+}
 
+struct NoShift_t {};
+
+template <std::uint32_t mask>
 class Field
 {
     std::uint32_t value_;
 
   public:
-    constexpr explicit Field(std::uint32_t value) noexcept : value_(value) {}
+    constexpr explicit Field(std::uint32_t value) noexcept
+        : Field(value << lsb(mask), NoShift_t{})
+    {}
+
+    constexpr explicit Field(std::uint32_t value, NoShift_t) noexcept
+        : value_(value)
+    {}
 
     constexpr auto value() const noexcept { return value_; };
+    static constexpr auto msk() noexcept -> std::uint32_t { return mask; }
+
+    template <std::uint32_t o_mask>
+    constexpr auto operator|(Field<o_mask> other_f) const noexcept
+    {
+        return Field<mask | o_mask>{value() | other_f.value(), NoShift_t{}};
+    }
 };
 
+template <std::uint32_t mask>
+constexpr auto fold(Field<mask> f)
+{
+    return f;
+}
+
+template <std::uint32_t mask, std::uint32_t... masks>
+constexpr auto fold(Field<mask> f, Field<masks>... fs)
+{
+    return f | fold(fs...);
+}
 
 class Register
 {
     std::uint32_t raw_;
 
   public:
-    auto write(Field f) noexcept -> void { raw_ = f.value(); }
+    template <std::uint32_t mask, std::uint32_t... masks>
+    auto write(Field<mask> f, Field<masks>... fs) noexcept -> void
+    {
+        const auto fields = fold(f, fs...);
+        raw_ = (raw_ & ~fields.msk()) | fields.value();
+    }
 };
 
 } // namespace regilite
--- test/read_write_unit.cpp
+++ test/read_write_unit.cpp
@@ -1,37 +1,40 @@
 #include "CppUTest/TestHarness.h"
 #include "register.hpp"
+#include "test_register.hpp"
 
 #include <cstdint>
 #include <type_traits>
 
-regilite::Register test_register;
+TestReg test_register;
 
-static_assert(std::is_standard_layout<decltype(test_register)>::value,
-              "Register<> type must always be standard layout.");
-
-std::uint32_t& register_view{*reinterpret_cast<std::uint32_t*>(&test_register)};
 
 TEST_GROUP(WriteToRegister)
 {
-    void setup() { register_view = 0u; };
+    void setup() { register_view(test_register) = 0u; };
 };
 
 TEST(WriteToRegister, SetOneBit)
 {
-    test_register.write(regilite::Field{1});
-    LONGS_EQUAL(0x1, register_view);
+    test_register.write(F0{1});
+    LONGS_EQUAL(0x1, register_view(test_register));
 }
 
 TEST(WriteToRegister, ClearOneBit)
 {
-    register_view = 1u;
-    test_register.write(regilite::Field{0});
-    LONGS_EQUAL(0x0, register_view);
+    register_view(test_register) = 1u;
+    test_register.write(F0{0});
+    LONGS_EQUAL(0x0, register_view(test_register));
 }
 
 TEST(WriteToRegister, SetTwoAdjacentFields)
 {
-    register_view = 1u;
-    test_register.write(regilite::Field{0});
-    LONGS_EQUAL(0x0, register_view);
+    test_register.write(F0{1}, F1{2});
+    LONGS_EQUAL(0b101, register_view(test_register));
+}
+
+TEST(WriteToRegister, WritesDoNotAffectBitsOutsideTheField)
+{
+    register_view(test_register) = 0b100u;
+    test_register.write(F0{1});
+    LONGS_EQUAL(0b101, register_view(test_register));
 }
