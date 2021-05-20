abd961117145afb848a1ef323cad54c9757d4a7e
Daniel Way <p.waydan@gmail.com>
2021-05-20 06:53:40 +0000

    Change TEST_GROUP name

--- test/write_field_unit.t.cpp
+++ test/write_field_unit.t.cpp
@@ -8,31 +8,31 @@
 TestReg test_register;
 
 
-TEST_GROUP(WriteToRegister)
+TEST_GROUP(WriteFieldsToRegister)
 {
     void setup() { register_view(test_register) = 0u; };
 };
 
-TEST(WriteToRegister, SetOneBit)
+TEST(WriteFieldsToRegister, SetOneBit)
 {
     test_register.write(F0{1});
     REGISTER_EQUALS(0x1, test_register);
 }
 
-TEST(WriteToRegister, SetTwoAdjacentFields)
+TEST(WriteFieldsToRegister, SetTwoAdjacentFields)
 {
     test_register.write(F0{1}, F1{2});
     REGISTER_EQUALS(0b101, test_register);
 }
 
-TEST(WriteToRegister, WritesOnlyModifyBitsInField)
+TEST(WriteFieldsToRegister, WritesOnlyModifyBitsInField)
 {
     register_view(test_register) = 0b100u;
     test_register.write(F0{1});
     REGISTER_EQUALS(0b101, test_register);
 }
 
-TEST(WriteToRegister, FieldUnsetBitsAreCleared)
+TEST(WriteFieldsToRegister, FieldUnsetBitsAreCleared)
 {
     register_view(test_register) = 0b1111;
     test_register.write(F1{1}, F0{0});
