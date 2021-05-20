ee8b3dc7024822178ab189ad73282539f0960be6
Daniel Way <p.waydan@gmail.com>
2021-05-20 06:44:08 +0000

    Add test field F2

--- test/test_register.hpp
+++ test/test_register.hpp
@@ -24,7 +24,7 @@ auto REGISTER_EQUALS(regilite::traits::identity_t<UInt> value,
 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 //  31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 15
 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |  |  |  |  |  |  |  |  |  |  |  |  |  |  F1 |F0|
+// |  |  |  |  |  |  |  |  |  |   F2   |  |  F1 |F0|
 // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 //  15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
 
@@ -33,8 +33,9 @@ using TestReg = regilite::Register<std::uint32_t>;
 static_assert(std::is_standard_layout<TestReg>::value,
               "Register<> type must always be standard layout.");
 
-using F0 = regilite::Field<std::uint32_t, 0b001>;
-using F1 = regilite::Field<std::uint32_t, 0b110>;
+using F0 = regilite::Field<std::uint32_t, 0b0000001>;
+using F1 = regilite::Field<std::uint32_t, 0b0000110>;
+using F2 = regilite::Field<std::uint32_t, 0b1110000>;
 
 
 #endif // TEST_REGISTER_HPP
