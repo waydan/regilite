14a83e650652c4bec6d4d9af82f76cd763b5682f
Daniel Way <p.waydan@gmail.com>
2021-05-20 06:58:47 +0000

    Add new test group

---
+++ test/read_field_unit.t.cpp
@@ -0,0 +1,7 @@
+#include "CppUTest/TestHarness.h"
+#include "register.hpp"
+#include "test_register.hpp"
+
+TEST_GROUP(ReadFieldFromRegister){};
+
+TEST(ReadFieldFromRegister, ReadSingleBitField) {}
