8d3b223306a59fe6b1991ebb2f1011ffd8515fb8
Daniel Way <p.waydan@gmail.com>
2021-06-21 07:01:57 +0000

    Added conjunction trait

--- src/traits.hpp
+++ src/traits.hpp
@@ -28,6 +28,18 @@ struct is_one_of<T, T, Ts...> {
 template <typename T, typename NotT, typename... Ts>
 struct is_one_of<T, NotT, Ts...> : is_one_of<T, Ts...> {};
 
+template <bool pred, bool... preds>
+struct conjunction;
+
+template <bool... preds>
+struct conjunction<false, preds...> : std::false_type {};
+
+template <bool... preds>
+struct conjunction<true, preds...> : conjunction<preds...> {};
+
+template <>
+struct conjunction<true> : std::true_type {};
+
 
 } // namespace traits
 } // namespace regilite
