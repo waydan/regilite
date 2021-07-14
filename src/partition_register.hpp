11a3f5fe655b7955b883ecab11704175df73d1c2
Daniel Way <p.waydan@gmail.com>
2021-07-14 06:48:59 +0000

    Implemented partitioned write

--- src/partition_register.hpp
+++ src/partition_register.hpp
@@ -1,7 +1,11 @@
 #ifndef REGILITE_PARTITION_REGISTER_HPP
 #define REGILITE_PARTITION_REGISTER_HPP
 
+#include "basicfield.hpp"
 #include "register_proxy.hpp"
+#include "traits.hpp"
+#include "utility.hpp"
+
 
 namespace regilite {
 
@@ -11,22 +15,31 @@ class Write
     static_assert(traits::is_storage_type<UInt>{},
                   "Write<> type requires an unsigned integral as its "
                   "underlying representation.");
+
+    std::uintptr_t addr_;
+
+  public:
+    explicit Write(const UInt* ptr)
+        : addr_(reinterpret_cast<std::uintptr_t>(ptr))
+    {}
+
+    constexpr auto address() -> std::uintptr_t { return addr_; }
 };
 
 template <int max_partition>
 struct Block;
 
 template <>
-struct Block<0> {
+struct Block<1> {
     using type = std::uint8_t;
 };
 template <>
-struct Block<1> {
+struct Block<2> {
     using type = std::uint16_t;
 };
 
-template <int msb>
-using Block_t = std::enable_if_t<(msb >= 0), typename Block<msb / 8>::type>;
+template <int msb, int lsb = 0>
+using Block_t = typename Block<(1 << (detail::msb((msb ^ lsb) / 8) + 1))>::type;
 
 
 namespace detail {
@@ -49,11 +62,17 @@ class PartitionRegisterImpl
     template <typename Field>
     auto write_field(Field f)
     {
-
-        const storage_type modified_state =
-            detail::insert_bits(volatile_read(), f);
-        volatile_write(modified_state);
-        return Write<Block_t<detail::msb(f.mask())>>{};
+        using WriteBlock =
+            Block_t<detail::msb(f.mask()), detail::lsb(f.mask())>;
+        constexpr int offset = detail::lsb(f.mask()) / (sizeof(WriteBlock) * 8);
+        constexpr int offset_bits = offset * sizeof(WriteBlock) * 8;
+        auto* state_segment_ptr =
+            reinterpret_cast<WriteBlock*>(&state_) + offset;
+        const WriteBlock modified_state =
+            (volatile_read(state_segment_ptr)
+             & ~static_cast<WriteBlock>(f.mask() >> offset_bits))
+            | static_cast<WriteBlock>(f.value() >> offset_bits);
+        return volatile_write(state_segment_ptr, modified_state);
     }
 
     auto volatile_write(storage_type s) noexcept -> void
@@ -61,11 +80,27 @@ class PartitionRegisterImpl
         *const_cast<volatile storage_type*>(&state_) = s;
     }
 
+    template <typename T>
+    auto volatile_write(T* address, T state) noexcept
+        -> std::enable_if_t<traits::is_storage_type<T>{}, Write<T>>
+    {
+        *const_cast<volatile T*>(address) = state;
+        return Write<T>{address};
+    }
+
     auto volatile_read() const noexcept -> storage_type
     {
         return *const_cast<const volatile storage_type*>(&state_);
     }
+
+    template <typename T>
+    auto volatile_read(T* address)
+        -> std::enable_if_t<traits::is_storage_type<T>{}, T>
+    {
+        return *const_cast<volatile T*>(address);
+    }
 };
+
 } // namespace detail
 
 template <typename UInt, typename AccessType, typename... MemberFields>
--- test/partition_register_unit.t.cpp
+++ test/partition_register_unit.t.cpp
@@ -12,7 +12,7 @@ TEST_GROUP(PartitionRegisterAccess)
     void setup() { register_view(test_register) = 0u; };
 };
 
-TEST(PartitionRegisterAccess, WriteSingleBit)
+TEST(PartitionRegisterAccess, WriteBitInLowestByte)
 {
     auto write_type = test_register.write(F0{1});
     static_assert(
@@ -28,4 +28,16 @@ TEST(PartitionRegisterAccess, WriteSpans16Bits)
         std::is_same<decltype(write_type), regilite::Write<std::uint16_t>>{},
         "Access the smallest aligned span which covers all fields.");
     REGISTER_EQUALS(0x101, test_register);
+}
+
+TEST(PartitionRegisterAccess, WriteOnlyUpperByte)
+{
+    auto access_audit = test_register.write(F3{F3Val::D});
+    static_assert(
+        std::is_same<decltype(access_audit), regilite::Write<std::uint8_t>>{},
+        "Access the smallest aligned span which covers all fields.");
+    REGISTER_EQUALS(0x800, test_register);
+
+    LONGS_EQUAL(reinterpret_cast<std::uintptr_t>(&test_register) + 1u,
+                access_audit.address());
 }
