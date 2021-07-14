ec34419bc51c0288e13200151ccb0c9c198c4fe4
Daniel Way <p.waydan@gmail.com>
2021-07-14 05:24:06 +0000

    Tests for PartionRegister

--- src/partition_register.hpp
+++ src/partition_register.hpp
@@ -4,20 +4,67 @@
 #include "register_proxy.hpp"
 
 namespace regilite {
+
+template <typename UInt>
+class Write
+{
+    static_assert(traits::is_storage_type<UInt>{},
+                  "Write<> type requires an unsigned integral as its "
+                  "underlying representation.");
+};
+
+template <int max_partition>
+struct Block;
+
+template <>
+struct Block<0> {
+    using type = std::uint8_t;
+};
+template <>
+struct Block<1> {
+    using type = std::uint16_t;
+};
+
+template <int msb>
+using Block_t = std::enable_if_t<(msb >= 0), typename Block<msb / 8>::type>;
+
+
 namespace detail {
 
 template <typename UInt, typename AccessType, typename... MemberFields>
 class PartitionRegisterImpl
 {
-    static_assert(std::is_unsigned<UInt>{} and not std::is_same<UInt, bool>{},
-                  "BasicRegister<> type requires an unsigned integral as its "
-                  "underlying representation.");
+    static_assert(
+        traits::is_storage_type<UInt>{},
+        "PartitionRegister<> type requires an unsigned integral as its "
+        "underlying representation.");
 
-  protected:
+  public:
     using storage_type = UInt;
 
   private:
     storage_type state_;
+
+  protected:
+    template <typename Field>
+    auto write_field(Field f)
+    {
+
+        const storage_type modified_state =
+            detail::insert_bits(volatile_read(), f);
+        volatile_write(modified_state);
+        return Write<Block_t<detail::msb(f.mask())>>{};
+    }
+
+    auto volatile_write(storage_type s) noexcept -> void
+    {
+        *const_cast<volatile storage_type*>(&state_) = s;
+    }
+
+    auto volatile_read() const noexcept -> storage_type
+    {
+        return *const_cast<const volatile storage_type*>(&state_);
+    }
 };
 } // namespace detail
 
---
+++ test/partition_register_unit.t.cpp
@@ -0,0 +1,30 @@
+#include <type_traits>
+
+#include "CppUTest/TestHarness.h"
+#include "test_registers.hpp"
+
+
+PartableReg test_register;
+
+
+TEST_GROUP(PartitionRegisterAccess)
+{
+    void setup() { register_view(test_register) = 0u; };
+};
+
+TEST(PartitionRegisterAccess, WriteSingleBit)
+{
+    auto write_type = test_register.write(F0{1});
+    static_assert(
+        std::is_same<decltype(write_type), regilite::Write<std::uint8_t>>{},
+        "Should only access the lowest byte");
+    REGISTER_EQUALS(0x1, test_register);
+}
+
+TEST(PartitionRegisterAccess, WriteSpans16Bits)
+{
+    auto write_type = test_register.write(F3{F3Val::A}, F0{1});
+    static_assert(
+        std::is_same<decltype(write_type), regilite::Write<std::uint16_t>>{},
+        "Access the smallest aligned span which covers all fields.");
+}
