c111a53c13d994b35b29a7f738ca9e108c590233
Daniel Way <p.waydan@gmail.com>
2021-10-20 02:15:10 +0000

    Check for prefix extraction when similar

--- regenerator/tests/cmsis_svd_parser/test_join_register_register.py
+++ regenerator/tests/cmsis_svd_parser/test_join_register_register.py
@@ -11,16 +11,30 @@ class TestRegisterJoining(unittest.TestCase):
     a = structuralModel.Register(name="a", size=8, reset_value=0)
     b = structuralModel.Register(name="b", size=8, reset_value=0)
 
-    def test_join_nonoverlapping_registers(self):
+    def test_struct_created_from_nonoverlapping_registers(self):
         self.assertEqual(
             cmsisSvdParser.joinMembers(self.a, 0, self.b, 1),
             (structuralModel.Struct(name="", members=[(self.a, 0), (self.b, 1)]), 0),
         )
 
-    def test_join_overlapping_registers(self):
+    def test_union_created_from_overlapping_registers(self):
         self.assertEqual(
             cmsisSvdParser.joinMembers(self.a, 0, self.b, 0),
             (structuralModel.Union(name="", members=[(self.a, 0), (self.b, 0)]), 0),
+        )
+
+    prefix_a = structuralModel.Register(name="prefix_a", size=8, reset_value=0)
+    prefix_b = structuralModel.Register(name="prefix_b", size=8, reset_value=0)
+
+    def test_struct_takes_common_prefix_as_name(self):
+        self.assertEqual(
+            cmsisSvdParser.joinMembers(self.prefix_a, 0, self.prefix_b, 1),
+            (
+                structuralModel.Struct(
+                    name="prefix", members=[(self.a, 0), (self.b, 1)]
+                ),
+                0,
+            ),
         )
 
 
