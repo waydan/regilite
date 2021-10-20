0cfeec2bb99b84c4fc4fd9da0169aa40ab609548
Daniel Way <p.waydan@gmail.com>
2021-10-20 02:15:36 +0000

    Test join structs and registers

---
+++ regenerator/tests/cmsis_svd_parser/test_join_struct_register.py
@@ -0,0 +1,50 @@
+"""
+Copyright 2021 Daniel Way
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import unittest
+from regenerator import cmsisSvdParser, structuralModel
+
+
+class TestStructRegisterJoining(unittest.TestCase):
+    a = structuralModel.Register(name="a", size=8, reset_value=0)
+    b = structuralModel.Register(name="b", size=8, reset_value=0)
+
+    def test_nonoverlapping_register_appended_to_members(self):
+        self.assertEqual(
+            cmsisSvdParser.joinMembers(
+                structuralModel.Struct(name="a_only", members=[(self.a, 0)]),
+                0,
+                self.b,
+                1,
+            ),
+            (
+                structuralModel.Struct(
+                    name="a_only", members=[(self.a, 0), (self.b, 1)]
+                ),
+                0,
+            ),
+        )
+
+    prefix_b = structuralModel.Register(name="prefix_b", size=8, reset_value=0)
+
+    def test_remove_register_name_prifix_if_matches_struct_name(self):
+        self.assertEqual(
+            cmsisSvdParser.joinMembers(
+                structuralModel.Struct(name="prefix", members=[(self.a, 0)]),
+                0,
+                self.prefix_b,
+                1,
+            ),
+            (
+                structuralModel.Struct(
+                    name="prefix", members=[(self.a, 0), (self.b, 1)]
+                ),
+                0,
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
