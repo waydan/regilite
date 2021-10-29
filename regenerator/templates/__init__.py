f51be97903456ea38fa61d7a7208dac0dced1a92
Daniel Way <p.waydan@gmail.com>
2021-10-29 13:45:41 +0000

    Bundle template data

---
+++ regenerator/templates/__init__.py
@@ -0,0 +1,15 @@
+from jinja2 import Template
+import os
+
+TEMPLATE_DIR = os.path.dirname(__file__)
+
+TEMPLATES = {
+    "peripheral": Template(open(f"{TEMPLATE_DIR}/peripheral.stub").read()),
+    "struct": Template(open(f"{TEMPLATE_DIR}/struct.stub").read()),
+    "union": Template(open(f"{TEMPLATE_DIR}/union.stub").read()),
+    "array": Template(open(f"{TEMPLATE_DIR}/array.stub").read()),
+    "register": Template(open(f"{TEMPLATE_DIR}/register.stub").read()),
+    "padding": Template(open(f"{TEMPLATE_DIR}/padding.stub").read()),
+    "decl_reg": Template(open(f"{TEMPLATE_DIR}/decl_register.stub").read()),
+    "field": Template(open(f"{TEMPLATE_DIR}/field.stub").read()),
+}
