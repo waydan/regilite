d758b5399d2cbc4d485079cbfa9ed186b0e862bd
Daniel Way <p.waydan@gmail.com>
2021-11-09 01:59:56 +0000

    Test for padding in struct

--- regenerator/tests/generator/test_generate_struct_member.py
+++ regenerator/tests/generator/test_generate_struct_member.py
@@ -4,10 +4,11 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import unittest
+from . import string_unittest_utils
 from regenerator import structuralModel, generateHeader
 
 
-class TestStructMemberGenerator(unittest.TestCase):
+class TestStructMemberGenerator(string_unittest_utils.TestCase):
     def test_generating_struct_member_without_registers(self):
         self.assertRegex(
             generateHeader.generate(structuralModel.Struct(name="s1")),
@@ -25,6 +26,37 @@ class TestStructMemberGenerator(unittest.TestCase):
                 )
             ),
             r"^struct\s+{\s*[\w:]+\sr1;\s*}\s*s1\s*;$",
+        )
+
+    def test_padding_added_between_noncontiguous_members(self):
+        test_struct = self.assertRegexExtractMatch(
+            generateHeader.generate(
+                structuralModel.Struct(
+                    name="ST",
+                    members=[
+                        (
+                            structuralModel.Register(
+                                name="R1",
+                                size=32,
+                                reset_value=0,
+                            ),
+                            0,
+                        ),
+                        (
+                            structuralModel.Register(
+                                name="R2",
+                                size=32,
+                                reset_value=0,
+                            ),
+                            8,
+                        ),
+                    ],
+                )
+            ),
+            r"(?s)^struct\s*{(?P<body>.*)}\s*ST\s*;",
+        )
+        self.assertRegex(
+            test_struct["body"], r"(?m)^[ \t]*regilite::padding<4>\s+_reserved_\d+"
         )
 
 
