45ff482213b7a6f7907b21089497cf26f00b1433
Daniel Way <p.waydan@gmail.com>
2021-11-17 00:00:40 +0000

    Refactor mbind to work with string and list types

--- regenerator/regenerator/utils.py
+++ regenerator/regenerator/utils.py
@@ -4,7 +4,20 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 from typing import Any, Callable
+from functools import singledispatch
 
 
+@singledispatch
 def mbind(value: Any, fn: Callable, default):
-    return fn(value) if value != None else default
+    return fn(value)
+
+
+@mbind.register(type(None))
+def _(value, fn: Callable, default):
+    return default
+
+
+@mbind.register(str)
+@mbind.register(list)
+def _(value, fn: Callable, default):
+    return fn(value) if value else default
