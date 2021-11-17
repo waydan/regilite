bb53ac083334571a076022930fba614718628480
Daniel Way <p.waydan@gmail.com>
2021-11-17 00:46:01 +0000

    Remove obsolete files

--- regenerator/templates/__init__.py
+++ regenerator/templates/__init__.py
@@ -5,10 +5,6 @@ TEMPLATE_DIR = os.path.dirname(__file__)
 
 TEMPLATES = {
     "peripheral": Template(open(f"{TEMPLATE_DIR}/peripheral.stub").read()),
-    "struct": Template(open(f"{TEMPLATE_DIR}/struct.stub").read()),
-    "union": Template(open(f"{TEMPLATE_DIR}/union.stub").read()),
-    "array": Template(open(f"{TEMPLATE_DIR}/array.stub").read()),
-    "register": Template(open(f"{TEMPLATE_DIR}/register.stub").read()),
     "padding": Template(open(f"{TEMPLATE_DIR}/padding.stub").read()),
     "decl_reg": Template(open(f"{TEMPLATE_DIR}/decl_register.stub").read()),
     "field": Template(open(f"{TEMPLATE_DIR}/field.stub").read()),
