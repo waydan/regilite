3bf276d0ee1184b72aa3361dffd22b49c125a423
Daniel Way <p.waydan@gmail.com>
2021-11-22 07:23:01 +0000

    Fix test dependency on mutable data

--- regenerator/tests/cmsis_svd_parser/test_join_data_members.py
+++ regenerator/tests/cmsis_svd_parser/test_join_data_members.py
@@ -4,21 +4,15 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import unittest
-
 from regenerator.parser import cmsissvd
 from regenerator.model import types, members
 
 
 Ra = types.Register(name="a", size=8)
 Rb = types.Register(name="b", size=8)
-prefix_a = members.DataMember(type=Ra, name="prefix_a", offset=0)
-prefix_b = members.DataMember(type=Rb, name="prefix_b", offset=1)
 
 
 class TestDataMemberJoining(unittest.TestCase):
-    a = types.Register(name="a", size=8)
-    b = types.Register(name="b", size=8)
-
     def test_struct_data_member_created_from_nonoverlapping_register_data_members(self):
         self.assertEqual(
             cmsissvd.smashMembers(
@@ -57,35 +51,35 @@ class TestDataMemberJoining(unittest.TestCase):
 
     def test_data_member_offsets_rebased_on_containing_member_offset(self):
         self.assertEqual(
-            cmsissvd.smashMembers(
-                members.DataMember(type=Ra, name="a", offset=4),
-                members.DataMember(type=Rb, name="b", offset=5),
+            tuple(
+                member.offset
+                for member in cmsissvd.smashMembers(
+                    members.DataMember(type=Ra, name="a", offset=4),
+                    members.DataMember(type=Rb, name="b", offset=5),
+                ).type.members
             ),
-            members.DataMember(
-                type=types.Struct(
-                    members=[
-                        members.DataMember(type=Ra, name="a", offset=0),
-                        members.DataMember(type=Rb, name="b", offset=1),
-                    ]
-                ),
-                name="",
-                offset=4,
-            ),
+            (0, 1),
         )
 
     def test_struct_data_member_takes_common_prefix_as_name(self):
         self.assertEqual(
-            cmsissvd.smashMembers(prefix_a, prefix_b),
-            members.DataMember(
-                type=types.Struct(
-                    members=[
-                        members.DataMember(type=Ra, name="a", offset=0),
-                        members.DataMember(type=Rb, name="b", offset=1),
-                    ]
-                ),
-                name="prefix",
-                offset=0,
+            cmsissvd.smashMembers(
+                members.DataMember(type=Ra, name="prefix_a", offset=0),
+                members.DataMember(type=Rb, name="prefix_b", offset=1),
+            ).name,
+            "prefix",
+        )
+
+    def test_member_name_prefix_removed_when_inserting_in_struct(self):
+        self.assertEqual(
+            tuple(
+                member.name
+                for member in cmsissvd.smashMembers(
+                    members.DataMember(type=Ra, name="prefix_a", offset=0),
+                    members.DataMember(type=Rb, name="prefix_b", offset=1),
+                ).type.members
             ),
+            ("a", "b"),
         )
 
 
