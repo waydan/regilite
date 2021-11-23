1269ef2e82fc941c129899c3624f3c569d2544cf
Daniel Way <p.waydan@gmail.com>
2021-11-23 23:53:22 +0000

    Simplify test script

--- regenerator/tests/cmsis_svd_parser/test_insert_member_into_struct.py
+++ regenerator/tests/cmsis_svd_parser/test_insert_member_into_struct.py
@@ -10,28 +10,27 @@ from regenerator.model import types, members
 
 
 Reg = types.Register(name="REG", size=8, reset_value=0)
-member_a = members.DataMember(type=Reg, name="a", offset=0)
-member_b = members.DataMember(type=Reg, name="b", offset=1)
-member_c = members.DataMember(type=Reg, name="c", offset=0)
+member_at_0 = members.DataMember(type=Reg, name="a", offset=0)
+member_at_1 = members.DataMember(type=Reg, name="b", offset=1)
 
 
 class TestJoiningStructWithDataMember(unittest.TestCase):
     def test_nonoverlapping_member_appended_to_member_list(self):
         self.assertEqual(
             cmsissvd.insertMember(
-                types.Struct(members=[member_a]),
-                member_b,
+                types.Struct(members=[member_at_0]),
+                member_at_1,
             ),
-            types.Struct(members=[member_a, member_b]),
+            types.Struct(members=[member_at_0, member_at_1]),
         )
 
     def test_overlapping_members_joined_and_appended_to_member_list(self):
         self.assertEqual(
             cmsissvd.insertMember(
-                types.Struct(members=[member_a]),
-                member_c,
+                types.Struct(members=[member_at_0]),
+                member_at_0,
             ),
-            types.Struct(members=[cmsissvd.smashMembers(member_a, member_c)]),
+            types.Struct(members=[cmsissvd.smashMembers(member_at_0, member_at_0)]),
         )
 
 
